fb9ead5d90c5f9a0a7ce2e46849a00a8a867f189
José Mauro Neto <jmauroneto@gmail.com>
2020-06-09 16:48:26 -0300

    Padronizando login e cadastro

--- src/components/SignUp/style.js
+++ src/components/SignUp/style.js
@@ -1,29 +1,30 @@
 import styled from 'styled-components';
 
 export const DivInterna = styled.div`
-width: 400px;
-box-sizing: border-box;
-height: 600px;
-margin: 5px;
-border: 1px solid black;
-background-color: #fff;
+  width: 400px;
+  box-sizing: border-box;
+  height: 600px;
+  margin: 5px;
+  border: 1px solid black;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 export const FormContainer = styled.form`
   display: flex;
   flex-direction: column; 
-  
+  width: 70%;
 `
-export const Titulo = styled.h2`
-  
-`
+
 export const CampoPreenchimento = styled.input`
    border: none;
    height:30px;
-   outline-color:black;
    border-bottom: 1px solid black;
-   margin-bottom: 2rem;  
-   max-width:100%;
+   margin-bottom: 32px;
+   width:100%;
    
    :valid {
      color: green;
@@ -40,6 +41,19 @@ export const Botao = styled.button`
     height: 30px;
     color: black;
     cursor: pointer;
-    
-  
+    align-self: center;
+`
+
+export const ImagemLogo = styled.img`
+  width: 70%;
+`
+
+export const TextoLogin = styled.p`
+  cursor: pointer;
+`
+
+export const MensagemErro = styled.p`
+  margin: 8px;
+  color: red;
+  align-self: center;
 `
